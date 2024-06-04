b03d9869d1b7e1fdfc02dc505e6b3d928807e9da
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-06-04 11:58:22 +0900

    Lesseon4ー3.3（Edit text)まで

--- app/src/main/java/jp/techacademy/kosuke/miyazaki/ui/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package jp.techacademy.kosuke.miyazaki.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log // 追加
+import android.view.View // 追加
 import jp.techacademy.kosuke.miyazaki.ui.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener { // 変更
 
     private lateinit var binding: ActivityMainBinding //追加
 
@@ -16,5 +19,19 @@ class MainActivity : AppCompatActivity() {
 
         //View BindingでUI部品を指定
         binding.button1.text = "ベンジャミンボタン"
+        binding.button1.setOnClickListener {
+            //EditTextの文字列をTextViewに設定
+            binding.textView.text = binding.editText.text.toString()
+        }
     }
+    // 以下の関数を追加
+    override fun onClick(v: View) {
+        Log.d("UI_PARTS", "ボタンをタップしました")
+
+    //以下を追加
+    binding.textView.text = "テキスト"
+
+    binding.textView.setTextColor(Color.MAGENTA)
+    }
+
 }
