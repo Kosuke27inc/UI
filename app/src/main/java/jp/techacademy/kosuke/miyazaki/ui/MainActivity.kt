c1b6bc89205ab491e980c2a8622cf7d4398ef0c9
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-06-04 23:19:56 +0900

    Lesseon4ー3.6（DatePickerDialog)まで *if-else構文

--- app/src/main/java/jp/techacademy/kosuke/miyazaki/ui/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/ui/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log // 追加
 import android.view.View // 追加
 import androidx.appcompat.app.AlertDialog
+import android.app.TimePickerDialog
+import android.app.DatePickerDialog
 import jp.techacademy.kosuke.miyazaki.ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener { // 変更
@@ -20,6 +22,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener { // 変更
 
         binding.button1.setOnClickListener(this)
         binding.button2.setOnClickListener(this)
+        binding.button3.setOnClickListener(this)
+        binding.button4.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -27,6 +31,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener { // 変更
             binding.textView.text = binding.editText.text.toString()
         } else if (v.id == R.id.button2) {
             showAlertDialog()
+        } else if (v.id == R.id.button3) {
+            showTimePickerDialog()
+        } else if (v.id == R.id.button4) {
+            showDatePickerDialog()
         }
     }
 
@@ -49,5 +57,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener { // 変更
 
         val alertDialog = alertDialogBuilder.create()
         alertDialog.show()
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                Log.d("UI_PARTS", "$hour:$minute")
+            },
+            13, 0, true)
+        timePickerDialog.show()
+    }
+
+    private fun showDatePickerDialog() {
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener() { view, year, month, dayOfMonth ->
+                Log.d("UI_PARTS", "$year/${month + 1}/$dayOfMonth")
+            },
+            2023,
+            0,
+            15
+        )
+        datePickerDialog.show()
     }
 }
